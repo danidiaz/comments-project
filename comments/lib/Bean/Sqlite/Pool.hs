d70bf516b8cc6cbca5628ca4f0039332e8ad175b
Daniel Díaz <diaz_carrete@yahoo.com>
2024-01-26 20:24:48 +0100

    Build the pool.

--- comments/lib/Bean/Sqlite/Pool.hs
+++ comments/lib/Bean/Sqlite/Pool.hs
@@ -3,18 +3,28 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Bean.Sqlite.Pool where
+{-# LANGUAGE NumDecimals #-}
+module Bean.Sqlite.Pool (SqlitePoolConf, SqlitePool, make) where
 
 import Data.Pool.Introspection
 import Sqlite
 import Data.Aeson
+import Data.Text
 import GHC.Generics (Generic)
+import Control.Monad.Catch (bracket)
 
-data SqliteConf = SqliteConf {
-    sqliteFile :: FilePath,
+data SqlitePoolConf = SqlitePoolConf {
+    databaseFile :: Text,
     poolSize :: Int
 }
   deriving stock (Generic)
   deriving anyclass (FromJSON, ToJSON)
 
 type SqlitePool = Pool Connection
+
+make :: forall r. SqlitePoolConf -> (SqlitePool -> IO r) -> IO r
+make SqlitePoolConf { databaseFile, poolSize } continuation = do
+    -- OpenV2NoMutex is "threaded" mode.
+    let openAction = Sqlite.openV2 DefaultVFS [OpenV2ExtendedResultCode, OpenV2NoMutex] OpenV2ReadWrite databaseFile
+    let poolConfig = defaultPoolConfig openAction Sqlite.close 3600 poolSize
+    bracket (newPool poolConfig) destroyAllResources continuation
--- comments/lib/Comments/Cauldron.hs
+++ comments/lib/Comments/Cauldron.hs
@@ -13,6 +13,7 @@ import Control.Monad.IO.Class
 import Data.Function ((&))
 import Log
 import Log.Backend.StandardOutput
+import Bean.Sqlite.Pool
 
 cauldron :: Cauldron Managed
 cauldron = do
@@ -22,6 +23,8 @@ cauldron = do
       let makeJsonConf = Bean.JsonConf.YamlFile.make do Bean.JsonConf.YamlFile.loadYamlSettings ["conf.yaml"] [] Bean.JsonConf.YamlFile.useEnv
       insert @(JsonConf IO) do makeBean do liftConIO do pack effect do makeJsonConf
     & insert @Logger do makeBean do pack effect do managed withStdOutLogger
+    & insert @SqlitePoolConf do makeBean do liftConIO do pack effect do Bean.JsonConf.lookupSection @IO "sqlite"
+    & insert @SqlitePool do makeBean do pack effect \conf -> managed do Bean.Sqlite.Pool.make conf
     & insert @CommentsServer do makeBean do pack value makeCommentsServer
     & insert @RunnerConf do makeBean do liftConIO do pack effect do Bean.JsonConf.lookupSection @IO "runner"
     & insert @Runner do makeBean do pack value makeRunner
